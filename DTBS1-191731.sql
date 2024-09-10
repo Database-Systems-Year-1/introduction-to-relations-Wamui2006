f82fef328cec76054e7fc17cf2d2fb6fe0fd8e78
Wamui2006 <167400864+Wamui2006@users.noreply.github.com>
2024-09-10 12:00:58 +0300

    Add files via upload

---
+++ DTBS1-191731.sql
@@ -0,0 +1,29 @@
+CREATE TABLE cars(
+brand VARCHAR(255),
+model VARCHAR(255),
+year INT
+);
+INSERT INTO cars(brand, model, year)
+VALUES
+('Ford','Mustang','1964'),
+('BMW','M1','1978'),
+('Toyota','Celica','1975');
+SELECT brand, year FROM cars;
+ALTER TABLE cars
+ADD color VARCHAR(255);
+SELECT*FROM cars;
+UPDATE cars
+SET color='red'
+WHERE brand='Ford';
+SELECT*FROM  cars;
+ALTER TABLE cars
+ALTER COLUMN year TYPE VARCHAR(4);
+ALTER TABLE cars
+DROP COLUMN color;
+SELECT*FROM CARS;
+DELETE FROM cars
+WHERE brand='BMW';
+SELECT*FROM cars;
+DROP TABLE cars;
+
+
